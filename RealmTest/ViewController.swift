4ccd1405267fca88c920afc5bc20b4aa4c534602
dit08 <dit@imac-3.local>
2019-05-29 15:12:50 +0900

    Initial Commit

---
+++ RealmTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealmTest
+//
+//  Created by dit08 on 2019. 5. 29..
+//  Copyright © 2019년 dit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
